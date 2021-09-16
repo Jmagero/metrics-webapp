c9fc090981ed87aaa2c41790e70c17a7a5d5295a
Jocyline Magero <jmagero2017@gmail.com>
2021-09-16 12:37:47 -0500

    Add dispatch action to get data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+/* eslint-disable */
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router';
+import { useSelector, useDispatch } from 'react-redux';
 import HomePage from './components/Homepage';
 import FilteredPage from './components/filteredPage';
+import { getData } from './redux/covid/covid';
+const  App = () => {
+  const state = useSelector((state) => state.covidReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
   return (
     <div className="App">
     <Switch>
@@ -11,7 +22,7 @@ import FilteredPage from './components/filteredPage';
         <FilteredPage />
       </Route>
     </Switch>
+  </div>
   );
 }
-
 export default App;
