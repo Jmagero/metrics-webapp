ee98bde4f00f8949ba198156e9194048f971c725
Jocyline Magero <jmagero2017@gmail.com>
2021-09-16 12:31:02 -0500

    Add routes and navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Switch, Route } from 'react-router';
+import HomePage from './components/Homepage';
+import FilteredPage from './components/filteredPage';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path={`/${state.country}`}>
+        <FilteredPage />
+      </Route>
+    </Switch>
   );
 }
 
